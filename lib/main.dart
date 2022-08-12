538b3be4a15d8c3d9f1dcd5a569a5cef5f950fa5
ily08 <ilyas_206@smkassalaambandung.sch.id>
2022-08-12 18:36:51 +0700

    test

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: GridView.count(
+        //widget yang akan ditampilkan dalam 1 baris adalah 2
+        crossAxisCount: 2,
+        children: [
+          //card ditampilkan disini
+          //saya membuat custom card di bawah agar kodingan tidak terlalu panjang
+          CustomCard(title: "Judul Card Kedua", image: ""),
+          CustomCard(title: "Judul Card Ketiga", image: ""),
+          CustomCard(title: "Judul Card Keempat", image: ""),
+          CustomCard(title: "Judul Card Kelima", image: ""),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
